54a40615ca4a3a3519ff190ba8d31242590ccddd
SeonJungKim <ksj083895@gmail.com>
2021-03-10 15:29:32 +0500

    Initial Commit

---
+++ SwiftUI_DisclousureGroup_TUT/SwiftUI_DisclousureGroup_TUTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DisclousureGroup_TUTApp.swift
+//  SwiftUI_DisclousureGroup_TUT
+//
+//  Created by 김선중 on 2021/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DisclousureGroup_TUTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
