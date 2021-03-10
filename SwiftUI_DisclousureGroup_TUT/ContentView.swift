21783a5b6ee1cccf9e190be6450589d7fa1df1d9
SeonJungKim <ksj083895@gmail.com>
2021-03-10 15:31:35 +0500

    텍스트

--- SwiftUI_DisclousureGroup_TUT/ContentView.swift
+++ SwiftUI_DisclousureGroup_TUT/ContentView.swift
@@ -1,16 +1,18 @@
-//
-//  ContentView.swift
-//  SwiftUI_DisclousureGroup_TUT
-//
-//  Created by 김선중 on 2021/03/10.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var Details = false
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            DisclosureGroup("embedded 란?", isExpanded: $Details) {
+                Text("embedded 는 영어로는 간직하다 , 끼워넣다 이런 뜻입니다. 프로그램 시스템의 일종으로  전자제품이나 전자 기기에 들어 가는 소규모의 마이크로 프로세서 시스템 이라고 보면 됩니다. 마이크로 프로세서는 운영 체제를 담는 그릇이라고 하면 ,  그런 운영 체제를 담고 있고,그에 따른 지배를 받는 마이크로 프로세서 시스템을 임베디드 시스템 이라고 합니다. 임베디드의 뜻은   embedded 라는 말에서도 알수 있듯이 내부에 무엇인가 포함하고 있는 상태를 말한다.")
+            }
             .padding()
+            Spacer()
+        }
     }
 }
 
